0d3260b46afabca4fb00e9871fbecad3a7b49fbf
MrSeaWave <774866545@qq.com>
2020-03-07 19:55:07 +0800

    chore: 升级 sea-koa-work-logger@0.0.4

--- src/service.js
+++ src/service.js
@@ -1,13 +1,14 @@
 import Koa from 'koa';
 import ip from 'ip';
-import { koaLogger } from 'sea-koa-work-logger';
+import { koaLogger, logOutside } from 'sea-koa-work-logger';
 import { startLog } from './config/debuggerConfig';
 import config from './utils/config';
 const { port } = config;
 const app = new Koa();
 
-app.use(koaLogger);
+app.use(koaLogger());
 app.use(async ctx => {
+  logOutside('hahhahahaha');
   ctx.body = 'Hello World';
 });
 
